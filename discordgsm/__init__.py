20dfccdba633b9923dfb91f7fca455e07cdd3d26
Joost <Joostv6@gmail.com>
2023-12-22 16:33:53 +0100

    .

--- discordgsm/__init__.py
+++
@@ -1,4 +0,0 @@
-# flake8: noqa
-from discordgsm.environment import env
-from discordgsm.logger import Logger
-from discordgsm.version import __version__
--- tests/test_import.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-def test_import():
-    import discordgsm.styles
-    import discordgsm.database
-    import discordgsm.gamedig
-    import discordgsm.logger
-    import discordgsm.main
-    import discordgsm.server
-    import discordgsm.service
